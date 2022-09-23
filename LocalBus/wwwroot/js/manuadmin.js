683cd31ae76b01a6e0d3ff7041fdc3c7aeeb3993
Israel Ribeiro <israel.ribeiro313@gmail.com>
2022-09-23 17:25:32 -0300

    algumas alterações no adm

---
+++ LocalBus/wwwroot/js/manuadmin.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
+
+$(document).ready(function () {
+
+    $('#dismiss').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
