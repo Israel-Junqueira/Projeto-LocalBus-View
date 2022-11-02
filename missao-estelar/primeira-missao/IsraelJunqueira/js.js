821958dc033c5b9cd70b4c8f262797641fdcf946
Israel Ribeiro <israel.ribeiro313@gmail.com>
2022-11-02 17:05:36 -0300

    teste

---
+++ missao-estelar/primeira-missao/IsraelJunqueira/js.js
@@ -0,0 +1,9 @@
+var missao = " missao estrelar js em execução"
+
+function eventoOnload() {
+    console.log("Evento onload disparado")
+}
+
+function eventoOnclick() {
+    console.log("missao estrelar realizada com sucesso")
+}
